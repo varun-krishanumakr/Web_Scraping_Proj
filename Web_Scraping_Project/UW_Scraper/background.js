158831e4baf5a0999c3c3389ee6bf3f5d2dd5bae
varun-krishanumakr <varkrishnakumar@gmail.com>
2026-01-01 16:46:03 -0800

    able to get the JSON for the planned courses page and find the planned courses

--- Web_Scraping_Project/UW_Scraper/background.js
+++ Web_Scraping_Project/UW_Scraper/background.js
@@ -4,9 +4,28 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "getPlannedCourses") {
     console.log("Background script received message to get planned courses.");
-    fetch("https://myplan.uw.edu/plan", {credentials: 'include'})
-      .then(response => response.text())
-      .then(html => sendResponse({html: html}));
+    fetch("https://plan-app-api.planning.sis.uw.edu/api/plan/terms", {credentials: 'include'})
+      .then(response => response.json())
+      .then(json => {
+        // Extract planned courses from the JSON response
+        const plannedCourses = {};
+        json.forEach(term => {
+          plannedListString = JSON.stringify(term.plannedList);
+          plannedListJSON = JSON.parse(plannedListString);
+          plannedListJSON.forEach(item => {
+            console.log("Planned list courses: ", item.courseDetails);
+          });
+          // plannedListJSON.forEach(item => {
+          //   console.log("Planned list courses: ", item.courseDetails);  
+          // })
+          // console.log(term.plannedList.courseDetails.abbrGenEdRequirements);
+          // term.plannedList.forEach(course => {
+          //   plannedCourses[course] = term.qtrYear;
+          // });
+        });
+        // console.log("Planned courses extracted:", plannedCourses);
+        // sendResponse({ plannedCourses });
+      });
     return true; // Keeps message channel open for async response
   }
 });
